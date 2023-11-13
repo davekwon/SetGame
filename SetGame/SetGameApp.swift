5823a37c5845bbee2e8ff051e36038798dd5150b
davekwon <dkwon714@gmail.com>
2023-11-12 20:14:28 -0800

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Dave Kwon on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
